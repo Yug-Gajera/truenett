9adbb93502daeeec4937910ec0cc5bfc8df146e5
Yug-Gajera <yuggajera66@gmail.com>
2026-07-09 18:22:18 +0530

    analytics

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,9 +1,6 @@
-import posthog from "posthog-js";
+import posthog from 'posthog-js'
 
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: "/ingest",
-  ui_host: "https://us.posthog.com",
-  defaults: "2026-01-30",
-  capture_exceptions: true,
-  debug: process.env.NODE_ENV === "development",
-});
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN!, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    defaults: '2026-05-30'
+})
